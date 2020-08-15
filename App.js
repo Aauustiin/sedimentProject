bd68b4c05572d0ef2bb1c38fed1842174162f8f4
Longaustin2001@gmail.com <Longaustin2001@gmail.com>
2020-08-15 15:52:17 +0100

    Basic data storage working.

--- App.js
+++ App.js
@@ -1,35 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage"
 
-const entry = {
-  title: "",
-  description: "",
-}
+const STORAGE_KEY = "example";
 
-const addEntry = async () => {
+const addEntry = async (title) => {
   try {
-    await AsyncStorage.setItem()
+    await AsyncStorage.setItem(STORAGE_KEY, title);
+    alert("Entry successfully saved.");
+  } catch(e) {
+    alert("Entry failed to save.");
   }
 }
 
-// We define the form on the initial screen where users can add an entry.
+const readEntry = async () => {
+  let x;
+  try {
+    x = await AsyncStorage.getItem(STORAGE_KEY);
+  } catch (e) {
+    x = "Error";
+    alert("Failed to fetch.")
+  }
+  return x;
+}
+
 const EntryForm = () => {
+  const [title, setTitle] = useState("Title");
+  const [description, setDescription] = useState("Description");
   return (
     <View>
       <TextInput
         style = {styles.TextInput}
-        defaultValue = "Title"
+        defaultValue = {title}
+        onChangeText = {title => setTitle(title)}
       />
       <TextInput
         style = {styles.TextInput}
-        defaultValue = "Description"
+        defaultValue = {description}
+        onChangeText = {description => setDescription(description)}
       />
       <Button
-        onPress={() => {
-          //Do Thing
-        }}
         title="Add Entry"
+        onPress={() => {
+          readEntry().then(function(result) {
+            console.log(result);
+          }, function(err) {
+            console.log(err);
+          })
+          addEntry(title);
+        }}
       />
     </View>
   );
